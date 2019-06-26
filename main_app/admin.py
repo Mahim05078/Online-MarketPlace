0bec9aad97b317b3f3dbb86835406789116f3634
miyad <38748105+miyad@users.noreply.github.com>
2019-06-26 04:28:37 -0400

    admin last

--- main_app/admin.py
+++ main_app/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Item, Shop, Customer, ShopItem, Shopowner, Shopassigned, RequestedRent
+from .models import Item, Shop, Customer, ShopItem, Shopowner, Shopassigned, RequestedRent, DeleivaryMan,Invoice, OrderedItem,SalesManager,Review
+
 
 
 admin.site.register(Item)
@@ -12,3 +13,10 @@ admin.site.register(ShopItem)
 admin.site.register(Shopassigned)
 admin.site.register(Shopowner)
 admin.site.register(RequestedRent)
+admin.site.register(DeleivaryMan)
+admin.site.register(Invoice)
+admin.site.register(OrderedItem)
+admin.site.register(SalesManager)
+admin.site.register(Review)
+
+
